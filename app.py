043e30927ae9e2e146334615aac277904e9c7379
Akinola Daniel <vantablack294@gmail.com>
2024-06-26 23:57:02 +0100

    notebook code for ft_model inference

--- app.py
+++ app.py
@@ -1,40 +1,65 @@
 import gradio as gr
 import whisper
 import os
+import pysrt
 import shutil
 import subprocess
 
-model = whisper.load_model("tiny") #replace with large-v3
+model = whisper.load_model("large-v3")  # replace with large-v3
 
-def srt_gen(video, tgt_lang):
-  if tgt_lang == "en":
-    result = model.transcribe(video, language=tgt_lang)
-  else:
-    pass
-  return result["text"]
 
-def srt_dl(video, tgt_lang):
-    result = model.transcribe(video)
-    srt_file_path = str("/tmp/gradio/")
-    writer = whisper.utils.get_writer("srt", srt_file_path)
-    writer(result, video)
+def srt_gen(video, src_lang, tgt_lang):
+    if src_lang == "en" and tgt_lang != "en":
+        translations = MultiInference(video, tgt_lang)
+        return translations
+    else:
+        result = model.transcribe(video, language=tgt_lang)
+        return result["text"]
+
+
+def srt_dl(video, src_lang, tgt_lang):
     video_name = os.path.basename(video).split(".")[0]
-    srt_file = f"{srt_file_path}/{video_name}.srt"
-    return srt_file
+    if src_lang == "en" and tgt_lang != "en":
+        subs = pysrt.SubRipFile()
+        translations = MultiInference(video, tgt_lang)
+        for i in range(len(translations["chunks"])):
+            start_time = translations["chunks"][i]["timestamp"][0]
+            end_time = translations["chunks"][i]["timestamp"][1]
+            text = translations["chunks"][i]["text"]
+            start = float_to_subriptime(start_time)
+            end = float_to_subriptime(end_time)
+            sub_item = pysrt.SubRipItem(
+                index=i, start=start, end=end, text=text)
+            subs.append(sub_item)
+            subs.save(f'/tmp/gradio/{video_name}.srt', encoding='utf-8')
+    else:
+        result = model.transcribe(video)
+        srt_file_path = str("/tmp/gradio/")
+        writer = whisper.utils.get_writer("srt", srt_file_path)
+        writer(result, video)
+        srt_file = f"{srt_file_path}/{video_name}.srt"
+        return srt_file
+
 
 with gr.Blocks() as demo:
+    gr.Markdown("# Automated Captions Generation")
     with gr.Row():
         with gr.Column():
             video = gr.Video(label="Upload Video")
-            tgt_lang = gr.Dropdown([("English", "en") , ("Hausa", "ha") , ("Yoruba", "yo")], label="Target Language")
+            src_lang = gr.Dropdown(
+                [("English", "en"), ("Hausa", "ha"), ("Yoruba", "yo")], label="Source Language")
+            tgt_lang = gr.Dropdown(
+                [("English", "en"), ("Hausa", "ha"), ("Yoruba", "yo")], label="Target Language")
             text_submit = gr.Button("Generate Text", variant="primary")
         with gr.Column():
-            text_output = gr.Text(label="Transcription", placeholder="Text output here.....")
+            text_output = gr.Text(label="Transcription",
+                                  placeholder="Text output here.....")
             srt_submit = gr.Button("Generate Subtitles", variant="primary")
             with gr.Column():
                 srt_output = gr.File(label="Subtitles")
 
-    text_submit.click(fn=srt_gen, inputs=[video, tgt_lang], outputs=[text_output])
+    text_submit.click(fn=srt_gen, inputs=[
+                      video, src_lang, tgt_lang], outputs=[text_output])
     srt_submit.click(fn=srt_dl, inputs=[video, tgt_lang], outputs=[srt_output])
 
 demo.launch(debug=True)
--- helper_funcs.py
+++ helper_funcs.py
@@ -18,38 +18,31 @@ def vidrip(video_file, output_ext="mp3"):
 
     return f"{filename}.{output_ext}"
 
+
 def float_to_subriptime(float_time):
-  hours = int(float_time)
-  minutes = int((float_time - hours) * 60)
-  seconds = int(((float_time - hours) * 60 - minutes) * 60)
-  return pysrt.SubRipTime(hours=hours, minutes=minutes, seconds=seconds)
+    hours = int(float_time)
+    minutes = int((float_time - hours) * 60)
+    seconds = int(((float_time - hours) * 60 - minutes) * 60)
+    return pysrt.SubRipTime(hours=hours, minutes=minutes, seconds=seconds)
 
 
+def MultiInference(video, tgt_lang):
+    peft_model_id = "laksf/laksf-merge-lora-final"
+    peft_config = PeftConfig.from_pretrained("laksf/whisper-large-v3-LORA")
+    model = WhisperForConditionalGeneration.from_pretrained(
+        peft_model_id, load_in_8bit=True, device_map="auto"
+    )
 
-def MultiInference(video,tgt_lang):
-  peft_model_id = "laksf/laksf-merge-lora-final"
-  peft_config = PeftConfig.from_pretrained("laksf/whisper-large-v3-LORA")
-  model = WhisperForConditionalGeneration.from_pretrained(
-    peft_model_id, load_in_8bit=True, device_map="auto"
-  )
-
-  task = "transcribe"
-  tokenizer = WhisperTokenizer.from_pretrained(peft_config.base_model_name_or_path, task=task)
-  processor = WhisperProcessor.from_pretrained(peft_config.base_model_name_or_path, task=task)
-  feature_extractor = processor.feature_extractor
-  pipe = pipeline("automatic-speech-recognition", model=model, tokenizer=tokenizer, feature_extractor=feature_extractor)
-  forced_decoder_ids = tokenizer.get_decoder_prompt_ids(language= tgt_lang, task="transcribe")
-  audio_file = vidrip(video)
-  translations = pipe(audio_file, return_timestamps= True, chunk_length_s=20, stride_length_s=(4,2),generate_kwargs={"forced_decoder_ids": forced_decoder_ids})
-
-  subs = pysrt.SubRipFile()
-
-  for i in range(len(translations["chunks"])):
-      start_time = translations["chunks"][i]["timestamp"][0]
-      end_time = translations["chunks"][i]["timestamp"][1]
-      text = translations["chunks"][i]["text"]
-      start = float_to_subriptime(start_time)
-      end = float_to_subriptime(end_time)
-      sub_item = pysrt.SubRipItem(index=i, start=start, end=end, text= text)
-      subs.append(sub_item)
-      subs.save('/content/output.srt', encoding='utf-8')
+    task = "transcribe"
+    tokenizer = WhisperTokenizer.from_pretrained(
+        peft_config.base_model_name_or_path, task=task)
+    processor = WhisperProcessor.from_pretrained(
+        peft_config.base_model_name_or_path, task=task)
+    feature_extractor = processor.feature_extractor
+    pipe = pipeline("automatic-speech-recognition", model=model,
+                    tokenizer=tokenizer, feature_extractor=feature_extractor)
+    forced_decoder_ids = tokenizer.get_decoder_prompt_ids(
+        language=tgt_lang, task="transcribe")
+    audio_file = vidrip(video)
+    translations = pipe(audio_file, return_timestamps=True, chunk_length_s=20, stride_length_s=(
+        4, 2), generate_kwargs={"forced_decoder_ids": forced_decoder_ids})
